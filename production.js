27c0870601ecf726878de2e9f4bb76fcd0d3f2d7
rekey <rekey@me.com>
2016-09-11 18:27:11 +0800

    production

---
+++ production.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Rekey on 2016/9/11.
+ */
+const log4js = require('./libs/log.js');
+const systemLogger = log4js.getLogger('system');
+const app = require('./app.js');
+
+app.proxy = true;
+
+// 加载中间件
+require('./middleware/index.js');
+// 加载路由
+require('./routers/index.js');
+
+app.listen(3000, '127.0.0.1');
+
+systemLogger.info('app', 'start', 'port', 3000);
